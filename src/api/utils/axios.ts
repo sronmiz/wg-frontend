c7a83f5012b4deb52ee3d977846490080abb5ef2
Ron Mizrahi <sronmiz@gmail.com>
2021-02-27 22:28:02 +0200

    fix refresh fallback

--- src/api/utils/axios.ts
+++ src/api/utils/axios.ts
@@ -9,15 +9,17 @@ export const axiosAgent: AxiosInstance = axios.create(agentConfig);
 
 const refreshToken = () => {
   const REFRESH_URL: string = `${config.BACKEND_URL}/auth/refresh`;
-  return axios.post(REFRESH_URL);
+  return axios.post(REFRESH_URL,agentConfig);
 };
 
 axiosAgent.interceptors.response.use(
   (response) => response,
   async (error) => {
+    const request = error.config
     if (error.response.status === 401) {
       try {
         await refreshToken();
+        return axios(error.config,agentConfig);
       } catch (error) {
         throw error;
       }
