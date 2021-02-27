50ce33d4fa89c837f6e77086dc76d632e6dbf9af
Ron Mizrahi <sronmiz@gmail.com>
2021-02-27 21:37:12 +0200

    fix refresh_token fallback

--- src/api/check-or-refresh-auth.ts
+++ src/api/check-or-refresh-auth.ts
@@ -2,12 +2,7 @@ import { axiosAgent } from "./utils";
 import { config } from "../config";
 
 const AUTH_URL = `${config.BACKEND_URL}/auth/`;
-const REFRESH_URL = `${config.BACKEND_URL}/auth/refresh`;
 
 export const checkOrRefreshAuth = async () => {
-  try {
-    return (await axiosAgent.post(AUTH_URL)).data;
-  } catch (err) {
-    return (await axiosAgent.post(REFRESH_URL)).data;
-  }
+  return (await axiosAgent.post(AUTH_URL)).data;
 };
--- src/api/utils/axios.ts
+++ src/api/utils/axios.ts
@@ -1,5 +1,25 @@
 import axios from "axios";
+import { config } from "../../config";
 
 export const axiosAgent = axios.create({
   withCredentials: true,
 });
+
+const refreshToken = () => {
+  const REFRESH_URL = `${config.BACKEND_URL}/auth/refresh`;
+  return axios.post(REFRESH_URL);
+};
+
+axiosAgent.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401) {
+      try {
+        await refreshToken();
+      } catch (error) {
+        throw error;
+      }
+    }
+  }
+);
